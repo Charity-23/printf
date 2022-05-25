e1f9a0e142f8f5d160eced5fafa29cec274548fe
Chizoba <upc4you@gmail.com>
2022-05-24 21:39:58 -0700

    added check for format presence

--- _printf.c
+++ _printf.c
@@ -1,49 +1,198 @@
 #include "main.h"
 
 /**
- * _printf - prints output to standard output
- * base on a format.
- * @format: the format of the string to print
- *
- * Return: number of characters printed except
- * null byte
+ * _strlen - calculate the length of string
+ * @str: string param
+ * Return: struct containing the len & fstring count
  */
 
-int _printf(const char *format, ...)
+struct cformat _strlen(char * str)
 {
-	int count = 0;
-	va_list args;
-	int (*function)(va_list) = NULL;
+	int len = 0;
+	int specifierscount = 0;
 
-	va_start(args, format);
-	
-	while (*format)
+	while (str[len] != '\0')
 	{
-		if (*format == '%' && *(format + 1) != '%')
+		if(str[len] == '%' && str[len + 1] != ' ')
 		{
-			format++;
-			function = get_function(format);
-			if (*(format) == '\0')
-				return (-1);
-			else if (function == NULL)
-			{
-				_putchar(*(format - 1));
-				_putchar(*format);
-				count += 2;
-			}
-			else
-				count += function(args);
+			specifierscount++;
 		}
-		else if (*format == '%' && *(format + 1) == '%')
-		{
-			_putchar(*format);
-			count++;
-		}
-
-		format++;
+		len++;
 	}
-	va_end(args);
-	return (count);
+	struct cformat lendesc;
+	lendesc.len = len;
+	lendesc.specifierscount = specifierscount;
+	return (lendesc);
 }
+
+/**
+ * _charcode - gets fcodes and their positions
+ * @str: string to be searched
+ * @count: number of fcodes
+ *
+ * Return: stuct containing fcodes and their positions
+ */
+
+struct fcode  _charcode(char * str, int count)
+{
+	int position = 0;
+	struct fcode dcode;
+	dcode.codes = malloc(sizeof(int) * count);
+	dcode.specifiercodes = malloc(sizeof(char) * count);
+	int insertPosition = 0;
+
+	while (str[position] != '\0')
+	{
+		if(str[position] == '%')
+		{
+			if(str[position+1] != ' ')
+			{
+				dcode.codes[insertPosition] = position;
+				dcode.specifiercodes[insertPosition] = str[position+1];
+				insertPosition++;
+			}
+		}
+		position++;
+	}
+	return (dcode);
+}
+
+/**
+ * insertstr - inserts a substring into another string
+ * @str: format string
+ * @start: start positin for intsert
+ * @strvar: string to be inserted
+ *
+ * Return: the new string from adding the two
+ */
+
+char * insertstr(char * str, int start, char * strvar)
+{
+	struct cformat strvarlen = _strlen(strvar);
+	struct cformat strlen = _strlen(str);
+
+	int charsize = strvarlen.len + strlen.len;
+	static char * charmem;
+	charmem = malloc(sizeof(char) * charsize);
+	int i = 0;
+	int l = 0;
+	int pause = 0;
+	
+	for(;i < charsize; i++)
+	{
+		if(i == start)
+		{
+			pause = i;
+
+			for(; l < strvarlen.len; l++)
+			{
+				charmem[i] = strvar[l];
+				i++;
+			}
+			pause += 2;
+		}
+		if(l != 0)
+		{
+			charmem[i] = str[pause];
+			pause++;
+		}
+		else
+		{
+			charmem[i] = str[i];
+		}
+	}
+		return (charmem);
+}
+
+/**
+ * _chartostring - convert char to string
+ * @c: char variable to convert
+ *
+ * Return: the resultant string
+ */
+
+char * _chartostring(char c)
+{
+	char * str = malloc(sizeof(char));
+	str[0] = (char) c;
+	return(str);
+}
+
+/**
+ * _printf - print formatted string
+ * @format: string to be formated & printed
+ *
+ * Retrun: number of bytes printed
+ */
+
+int _printf(char * format, ...)
+{
+	if (!format)
+		return (-1);
+
+	struct cformat fdetails = _strlen(format);
+	struct fcode dcode = _charcode(format, fdetails.specifierscount);
+	int * codeptr = dcode.codes;
+	char* str1;
+	char str2;
+
+	va_list ptr;
+	va_start(ptr, format);
+	for (int i = 0; i < fdetails.specifierscount; i++)
+	{
+		if(dcode.specifiercodes[i] == 's')
+		{
+			str1 = va_arg(ptr, char*);
+		}
+		else if(dcode.specifiercodes[i] == 'c')
+		{
+			str2 = va_arg(ptr, int);
+		}
+		else
+		{
+			continue;
+		}
+	}
+	va_end(ptr);
+
+	char * newstr = format;
+	int chardone = 0;
+	int stringdone = 0;
+	int percentdone = 0;
+
+	for(int j = 0; j < fdetails.specifierscount; j++)
+	{
+		if(dcode.specifiercodes[j] == 's' && !stringdone)
+		{
+			dcode = _charcode(newstr,fdetails.specifierscount);
+			codeptr = dcode.codes;
+			newstr = insertstr(newstr, *codeptr, str1);
+			fdetails.specifierscount--;
+			stringdone = 1;
+		}
+		if(dcode.specifiercodes[j] == 'c' && !chardone)
+		{
+			char * strarr = _chartostring(str2);
+			codeptr = dcode.codes;
+			newstr = insertstr(newstr, *(codeptr), strarr);
+			fdetails.specifierscount--;
+			chardone = 1;
+		}
+		if(dcode.specifiercodes[j] == '%' && !percentdone)
+		{
+			char * str3 = "%";
+
+			dcode = _charcode(newstr,fdetails.specifierscount);
+			codeptr = dcode.codes;
+			newstr = insertstr(newstr, *(codeptr), str3);
+			fdetails.specifierscount--;
+			percentdone = 1;
+		}
+	}
+	struct cformat newstrlen = _strlen(newstr);
+	write(2, newstr, newstrlen.len);
+	return (newstrlen.len);
+}
+
 
 
